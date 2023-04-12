32e5fdb55e6fc5080a6b5bc2bdf6a83097069a19
nik-agarwal <67484380+nik-agarwal@users.noreply.github.com>
2023-04-12 16:49:36 +0530

    Add files via upload

---
+++ laptopWebsite/account_engine/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('shop/', views.shop, name='shop'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('single-product/', views.single_product, name='single_product'),
+    path('cart/', views.cart, name='cart'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('login-Signup/', views.login_Signup, name='login_Signup'),
+
+]
